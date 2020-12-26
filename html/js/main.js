8a0b96199f3532fd28264feebdaed76884cf75e2
TrungHieu1912 <52956674+TrungHieu1912@users.noreply.github.com>
2020-12-26 18:48:54 +0700

    update 1

---
+++ html/js/main.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 20
+function myFunction() {
+    // console.log(a + b);
+    // alert('alo');
+    var arr = ['giá trị là 1', 'giá trị là 2', 'giá trị là 3']
+    var a = prompt('Nhập vào số trong khoảng từ 0 đến 3');
+    console.log('giá trị của a là', a);
+    console.log(arr[a]);
+}
